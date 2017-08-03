c8d1a636415d855b26398af63bf3f1b47735f022
Aditya Gupta <contactadityag@gmail.com>
2017-08-03 19:07:39 -0400

    Code fix, cleaning up and adding comments.

--- src/ADBFinalProject/ReplicatedConcurrencyControlAndRecovery.java
+++ src/ADBFinalProject/ReplicatedConcurrencyControlAndRecovery.java
@@ -27,17 +27,17 @@ public class ReplicatedConcurrencyControlAndRecovery {
 	  int initialValue = 10 * dataNumber;
 	  
 	  if (dataNumber % 2 != 0) {
-		// Initialize odd indexed data.
-		boolean isReplicated = false;
-		int siteNumberToInsert = dataNumber % numberOfSites;
-		transactionManager.initializeDataAtSite(siteNumberToInsert, dataName, initialValue, isReplicated);
+	     // Initialize odd indexed data.
+	     boolean isReplicated = false;
+	     int siteNumberToInsert = dataNumber % numberOfSites;
+	     transactionManager.initializeDataAtSite(siteNumberToInsert, dataName, initialValue, isReplicated);
 	  }
 	  else {
-		// Initialize even indexed data.
-		boolean isReplicated = true;
-		for (int site = 0; site < numberOfSites; site++) {
-		  transactionManager.initializeDataAtSite(site, dataName, initialValue, isReplicated);
-		}
+	     // Initialize even indexed data.
+	     boolean isReplicated = true;
+	     for (int site = 0; site < numberOfSites; site++) {
+	        transactionManager.initializeDataAtSite(site, dataName, initialValue, isReplicated);
+	     }
 	  }
 	}
 
@@ -48,7 +48,7 @@ public class ReplicatedConcurrencyControlAndRecovery {
 	  File inputFile = new File(args[0]);
 	  input = new Scanner(inputFile);
 	} else {
-      input = new Scanner(System.in);
+      	  input = new Scanner(System.in);
 	}
 	
 	// While there are more commands, read a line of input, parse, call appropriate handler.
@@ -59,61 +59,56 @@ public class ReplicatedConcurrencyControlAndRecovery {
 	  String inputLine = input.nextLine();
 	  String[] commands = inputLine.replaceAll("\\s+", "").split(";");
 	  for (int currentCommand = 0; currentCommand < commands.length; currentCommand++) {
-		String command = commands[currentCommand];
-		String[] commandTokens = command.split("[,\\(\\)]");
-		String commandType = commandTokens[0];
-		switch(commandType) {
-		  case "begin":
+	     String command = commands[currentCommand];
+	     String[] commandTokens = command.split("[,\\(\\)]");
+	     String commandType = commandTokens[0];
+	     switch(commandType) {
+	        case "begin":
 			String transactionName = commandTokens[1];
-			int transactionNumber = Integer.parseInt(
-			    transactionName.substring(1, transactionName.length()));
+			int transactionNumber = Integer.parseInt(transactionName.substring(1, transactionName.length()));
 			transactionManager.begin(time, transactionNumber);
 			break;
-		  case "beginRO":
+		case "beginRO":
 			transactionName = commandTokens[1];
-			transactionNumber = Integer.parseInt(
-			    transactionName.substring(1, transactionName.length()));
+			transactionNumber = Integer.parseInt(transactionName.substring(1, transactionName.length()));
 			transactionManager.beginRO(time, transactionNumber);
 			break;
-		  case "R":
+		case "R":
 			transactionName = commandTokens[1];
-			transactionNumber = Integer.parseInt(
-				transactionName.substring(1, transactionName.length()));
+			transactionNumber = Integer.parseInt(transactionName.substring(1, transactionName.length()));
 			String dataName = commandTokens[2];
 			transactionManager.read(time, transactionNumber, dataName);
 			break;
-		  case "W":
+		case "W":
 			transactionName = commandTokens[1];
-		    transactionNumber = Integer.parseInt(
-			  transactionName.substring(1, transactionName.length()));
-		    dataName = commandTokens[2];
-		    int value = Integer.parseInt(commandTokens[3]);
-		    transactionManager.write(time, transactionNumber, dataName, value);
+		        transactionNumber = Integer.parseInt(transactionName.substring(1, transactionName.length()));
+		    	dataName = commandTokens[2];
+		    	int value = Integer.parseInt(commandTokens[3]);
+		    	transactionManager.write(time, transactionNumber, dataName, value);
 			break;
-		  case "dump":
+		case "dump":
 			if (commandTokens.length == 1) {
 			  transactionManager.dump();
 			} else if (commandTokens[1].length() == 1) {
-		      transactionManager.dump(1);
+		      	  transactionManager.dump(1);
 			} else if (commandTokens[1].length() == 2) {
-		      transactionManager.dump(commandTokens[1]);
+		      	  transactionManager.dump(commandTokens[1]);
 			} else {
 			  throw new Exception("Invalid arguments to dump!");
 			}
 			break;
-		  case "end":
+		case "end":
 			transactionName = commandTokens[1];
-            transactionNumber = Integer.parseInt(
-		        transactionName.substring(1, transactionName.length()));
-            transactionManager.end(time, transactionNumber);
+            		transactionNumber = Integer.parseInt(transactionName.substring(1, transactionName.length()));
+            		transactionManager.end(time, transactionNumber);
 			break;
-		  case "fail":
+		case "fail":
 			transactionManager.fail(time, Integer.parseInt(commandTokens[1]));
 			break;
-		  case "recover":
+	        case "recover":
 			transactionManager.recover(time, Integer.parseInt(commandTokens[1]));
 			break;
-		  default:
+		default:
 			throw new Exception("Invalid command type!");
 		}
 	  }
